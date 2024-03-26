eca746a46d04c03fb1ed8e82330fd2e41c8a1af3
Shivesh Lochunah <slochunah30@gmail.com>
2024-03-26 10:39:28 +0000

    structural changes

---
+++ src/system.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import { useState } from "react";
+
+export const System = ({}) => {
+  return (
+    <div className="system">
+      <div className="navbar"></div>
+      <header className="dashboard-header"></header>
+      <main className="dashboard-main"></main>
+    </div>
+  );
+};
