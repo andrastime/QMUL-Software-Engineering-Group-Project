38f5abcc7059b2795ac63eaad30c0155fdb819b5
Shivesh Lochunah <slochunah30@gmail.com>
2024-03-28 21:55:06 +0000

    login page completed

--- src/system.jsx
+++ src/system.jsx
@@ -1,12 +1,23 @@
 import "./index.css";
-import { useState } from "react";
+import { createClient } from '@supabase/supabase-js';
 import { StudentFeedback } from "./components";
 
-export const System = ({}) => {
+const supabaseUrl = 'https://dswpmnhkvgpxqapgddfe.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd3BtbmhrdmdweHFhcGdkZGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MjYzNTgsImV4cCI6MjAyNjAwMjM1OH0.IBBT3bh87_nDHckwlR434DuHI1UvcoVypfcJH90s4eA';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const System = () => {
+  // Function to handle the logout process
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error("Logout error", error.message);
+  };
+
   return (
     <div className="system">
       <div className="navbar">
-        <img src="/notification.svg" alt="Notification" c />
+        <button onClick={handleLogout} className="logout-button">Logout</button> {/* Logout Button */}
+        <img src="/notification.svg" alt="Notification" />
       </div>
       <header className="dashboard-header"></header>
       <main className="dashboard-main"></main>
