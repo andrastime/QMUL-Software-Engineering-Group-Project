7547bcb6b30d5e64b1288a73041590f15508f20a
Shivesh Lochunah <slochunah30@gmail.com>
2024-03-28 19:33:16 +0000

    login code added

--- src/system.jsx
+++ src/system.jsx
@@ -5,7 +5,9 @@ import { StudentFeedback } from "./components";
 export const System = ({}) => {
   return (
     <div className="system">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <img src="/notification.svg" alt="Notification" c />
+      </div>
       <header className="dashboard-header"></header>
       <main className="dashboard-main"></main>
       <StudentFeedback /> {/* temporarily put here to display it */}
